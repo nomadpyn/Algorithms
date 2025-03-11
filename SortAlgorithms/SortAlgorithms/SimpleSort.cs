e931c094b1eaaf646e94515c1b7fbbfce8be894c
nomadpyn <105908089+nomadpyn@users.noreply.github.com>
2025-03-11 18:25:56 +0700

    Add SimpleSort

---
+++ SortAlgorithms/SortAlgorithms/SimpleSort.cs
@@ -0,0 +1,36 @@
+﻿
+namespace SortAlgorithms.SortAlgorithms
+{
+    /// <summary>
+    /// Алгоритм простой сортировки - O(N*N)
+    /// </summary>
+    public static class SimpleSort
+    {
+        public static void Sort(int[] arr)
+        {
+            int temp;
+            int minIndex;
+
+            for(int i =0; i < arr.Length - 1; i++)
+            {
+                temp = arr[i];
+                minIndex = i;
+
+                for(int j = i+1; j< arr.Length; j++)
+                {
+                    if (arr[j] < temp)
+                    {
+                        temp = arr[j];
+                        minIndex = j;
+                    }
+                }
+
+                if (i != minIndex)
+                {
+                    arr[minIndex] = arr[i];
+                    arr[i] = temp;
+                }
+            }
+        }
+    }
+}
