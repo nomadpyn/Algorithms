392814f7a0a05cde17c90f9a7a98e30aada80856
nomadpyn <105908089+nomadpyn@users.noreply.github.com>
2025-03-11 11:38:17 +0700

    Add UnSortSearchTests

---
+++ SearchAlgorithmsTests/MoqData.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchAlgorithmsTests
+{
+    internal static class MoqData
+    {
+        #region Private Properties
+
+        private static readonly int[] fullArr = [656, 3663, 7456, 41, 778, 425, 7876, 2355, 9989, 237];
+
+        private const int inFullArr = 425;
+
+        private const int inFullArrIndex = 5;
+
+        private const int notInFullArr = 5475;
+
+        private const int badIndex = -1;
+
+        #endregion
+
+        #region Internal Methods
+
+        /// <summary>
+        /// Набор данных для корректного поиска
+        /// </summary>
+        /// <returns></returns>
+        internal static (int[] arr , int value , int index) GetCorrectSearchData()
+        {
+            return (fullArr, inFullArr, inFullArrIndex);
+        }
+
+        /// <summary>
+        /// Набор данных для поиска без результата
+        /// </summary>
+        /// <returns></returns>
+        internal static (int[] arr, int value, int index) GetBadSearchData()
+        {
+            return (fullArr, notInFullArr, -1);
+        }
+
+        /// <summary>
+        /// Набор данных для пустого массива
+        /// </summary>
+        /// <returns></returns>
+        internal static (int[] arr, int value, int index) GetEmptySearchData()
+        {
+            return ([], inFullArr, -1);
+        }
+
+        #endregion
+    }
+}
---
+++ SearchAlgorithmsTests/UnSortSearchTests.cs
@@ -0,0 +1,80 @@
+﻿#region Usings
+using SearchAlgorithms.SearchAlgorithms;
+#endregion
+
+namespace SearchAlgorithmsTests
+{
+    /// <summary>
+    /// Тесты для поиска несортированного массива 
+    /// </summary>
+    public class UnSortSearchTests
+    {
+        #region SimpleSearch
+
+        [Fact]
+        public void SimpleSearchInArr()
+        {
+            var moq = MoqData.GetCorrectSearchData();
+
+            int result = UnSortSearch.SimpleSearch(moq.arr, moq.value);
+
+            Assert.True(result == moq.index);
+        }
+
+        [Fact]
+        public void SimpleSearchNotInArr()
+        {
+            var moq = MoqData.GetBadSearchData();
+
+            int result = UnSortSearch.SimpleSearch(moq.arr, moq.value);
+
+            Assert.True(result == moq.index);
+        }
+
+        [Fact]
+        public void SimpleSearchEmpyArr()
+        {
+            var moq = MoqData.GetEmptySearchData();
+
+            int result = UnSortSearch.SimpleSearch(moq.arr, moq.value);
+
+            Assert.True(result == moq.index);
+        }
+
+        #endregion
+
+        #region BarrierSearch
+
+        [Fact]
+        public void BarrierSearchInArr()
+        {
+            var moq = MoqData.GetCorrectSearchData();
+
+            int result = UnSortSearch.SimpleSearch(moq.arr, moq.value);
+
+            Assert.True(result == moq.index);
+        }
+
+        [Fact]
+        public void BarrierSearchNotInArr()
+        {
+            var moq = MoqData.GetBadSearchData();
+
+            int result = UnSortSearch.SimpleSearch(moq.arr, moq.value);
+
+            Assert.True(result == moq.index);
+        }
+
+        [Fact]
+        public void BarrierSearchEmpyArr()
+        {
+            var moq = MoqData.GetEmptySearchData();
+
+            int result = UnSortSearch.SimpleSearch(moq.arr, moq.value);
+
+            Assert.True(result == moq.index);
+        }
+
+        #endregion
+    }
+}
