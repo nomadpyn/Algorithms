2c187a41ca37610f6feb37fa89873917b440bd7a
nomadpyn <105908089+nomadpyn@users.noreply.github.com>
2025-03-11 11:58:13 +0700

    Add UnSortSearch Examples

---
+++ Algorithms/Services/CustomSW.cs
@@ -0,0 +1,46 @@
+﻿#region Usings
+using System.Diagnostics;
+#endregion
+
+namespace Algorithms.Services
+{
+    /// <summary>
+    /// Кастом StopWatch для выполнения делегата
+    /// </summary>
+    public class CustomSW
+    {
+        #region Private Properties
+
+        private readonly Action action;
+
+        private readonly Stopwatch stopWatch;
+
+        #endregion
+
+        #region Constructors
+
+        public CustomSW(Action action)
+        {
+            this.stopWatch = new Stopwatch();
+            this.action = action;
+
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public void ShowResult()
+        {
+            stopWatch.Start();
+
+            action.Invoke();
+
+            stopWatch.Stop();
+
+            Console.WriteLine($"Время выполнения {stopWatch.ElapsedMilliseconds} мс");
+        }
+
+        #endregion
+    }
+}
