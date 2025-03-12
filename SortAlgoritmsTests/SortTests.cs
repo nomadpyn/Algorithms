53d1ff77f33bd0bee07a69a5bedecc859f3dd669
nomadpyn <105908089+nomadpyn@users.noreply.github.com>
2025-03-12 19:01:34 +0700

    Add tests SimpleInsertion

---
+++ SortAlgoritmsTests/SortTests.cs
@@ -0,0 +1,31 @@
+using SortAlgorithms.SortAlgorithms;
+
+namespace SortAlgoritmsTests
+{
+    public class SortTests
+    {
+        [Fact]
+        public void SimpleSortTest()
+        {
+            var arr1 = MoqData.GetIntArray();
+
+            var arr2 = MoqData.GetSortArray();
+
+            SimpleSort.Sort(arr1);
+
+            Assert.Equal(arr2, arr1);
+        }
+
+        [Fact]
+        public void SimpleInclusionSortTest()
+        {
+            var arr1 = MoqData.GetIntArray();
+
+            var arr2 = MoqData.GetSortArray();
+
+            InclusionSort.SimpleInsertion(arr1);
+
+            Assert.Equal(arr2, arr1);
+        }
+    }
+}
