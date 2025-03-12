82947117291763b451f22f6a997f5184956d2581
nomadpyn <105908089+nomadpyn@users.noreply.github.com>
2025-03-12 19:01:22 +0700

    Add SimpleInsertion sort

---
+++ SortAlgorithms/SortAlgorithms/InclusionSort.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortAlgorithms.SortAlgorithms
+{
+    /// <summary>
+    /// Алгоритмы сортировки включениями - O(N*N)
+    /// </summary>
+    public static class InclusionSort
+    {
+        /// <summary>
+        /// Сортировка простым включением 
+        /// </summary>
+        /// <param name="arr"></param>
+        public static void SimpleInsertion(int[] arr)
+        {            
+
+            for(int i=1; i < arr.Length; i++)
+            {
+                int temp = arr[i];
+
+                int j = i - 1;
+
+                while(j >= 0 && arr[j] > temp)
+                {
+                    arr[j + 1] = arr[j--];
+                }
+
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
