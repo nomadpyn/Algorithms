f5ae1b0ef14fe1e17cf02f4a5d2a31e11802f162
nomadpyn <105908089+nomadpyn@users.noreply.github.com>
2025-03-12 19:10:35 +0700

    Add BinaryInclusion

--- SortAlgorithms/SortAlgorithms/InclusionSort.cs
+++ SortAlgorithms/SortAlgorithms/InclusionSort.cs
@@ -16,20 +16,56 @@ namespace SortAlgorithms.SortAlgorithms
         /// </summary>
         /// <param name="arr"></param>
         public static void SimpleInsertion(int[] arr)
-        {            
+        {
 
-            for(int i=1; i < arr.Length; i++)
+            for (int i = 1; i < arr.Length; i++)
             {
                 int temp = arr[i];
 
                 int j = i - 1;
 
-                while(j >= 0 && arr[j] > temp)
+                while (j >= 0 && arr[j] > temp)
                 {
                     arr[j + 1] = arr[j--];
                 }
 
                 arr[j + 1] = temp;
+            }
+        }
+
+        /// <summary>
+        /// Сортировка бинарным включением
+        /// </summary>
+        /// <param name="arr"></param>
+        public static void BinaryInclusion(int[] arr)
+        {
+            for (int i = 1; i < arr.Length; i++)
+            {
+                int temp = arr[i];
+                int left = 0;
+                int right = i - 1;
+
+                while (left <= right)
+                {
+                    int m = (left + right) / 2;
+
+                    if (temp < arr[m])
+                    {
+                        right = m - 1;
+                    }
+                    else
+                    {
+                        left = m + 1;
+                    }
+                }
+
+                for (int j = i - 1; j >= left; j--)
+                {
+                    arr[j + 1] = arr[j];
+                }
+
+                arr[left] = temp;
+
             }
         }
     }
