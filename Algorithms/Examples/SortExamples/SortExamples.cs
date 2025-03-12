2fc0ce7316a9c5e6139c4cb951e3dd65b9945b3d
nomadpyn <105908089+nomadpyn@users.noreply.github.com>
2025-03-12 19:51:30 +0700

    Add ExchangeSortExamples

--- Algorithms/Examples/SortExamples/SortExamples.cs
+++ Algorithms/Examples/SortExamples/SortExamples.cs
@@ -21,19 +21,44 @@ namespace Algorithms.Examples.SortExamples
 
         #region Public Methods
 
+        /// <summary>
+        /// Пример простой сортировки 
+        /// </summary>
         public void SimpleSortExample()
         {
             SimpleSort.Sort(data);
         }
 
+        /// <summary>
+        /// Пример простой сортировки вставками 
+        /// </summary>
         public void InsertionSimpleSortExample()
         {
             InclusionSort.SimpleInsertion(data);
         }
 
+        /// <summary>
+        /// Пример бинарной сортировки вставками
+        /// </summary>
         public void InclusionBinarySortExample()
         {
             InclusionSort.BinaryInclusion(data);
+        }
+
+        /// <summary>
+        /// Пример сортировки пузырьком
+        /// </summary>
+        public void ExchangeBubbleSortExample()
+        {
+            ExchangeSort.BubbleSort(data);
+        }
+
+        /// <summary>
+        /// Пример сортировки шейкером
+        /// </summary>
+        public void ExchangeShakerSortExample()
+        {
+            ExchangeSort.ShakerSort(data);
         }
 
         #endregion
